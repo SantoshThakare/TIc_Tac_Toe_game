bab6f1d90949022b45d88924312c16e89e4f9f8b
SantoshThakare <santoshthakare50@gmail.com>
2022-02-10 11:12:28 +0530

    [santosh]: Enter X or O

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using tic_tac_board;
+
 Console.WriteLine("Hello, World!");
+Tic_Tac_toe tic_ = new Tic_Tac_toe();
+tic_.Tac();
+tic_.inputXorO();
--- Tic_Tac_toe.cs
+++ Tic_Tac_toe.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace tic_tac_board
 {
-    internal class Tic_Tac_toe
-    {
+    public class Tic_Tac_toe
+    {    
+        public void Tac()
+        {
+            Console.WriteLine("Welcome to TicTacToe");
+            char[] grid = new char[10];
+
+            for (int i = 1; i < 10; i++)
+            {
+                grid[i] = ' ';
+            }
+        }
+        public void inputXorO()
+        {
+            Console.WriteLine("Enter  the X or O");
+            char entry = Convert.ToChar(Console.ReadLine());
+        }
     }
+        
 }
